cbd2c77dc3e14861f540a576273f325a4aad1951
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-01-08 21:52:01 +0600

    Add files via upload

---
+++ Lab 01/1. 353_50-6.cpp
@@ -0,0 +1,17 @@
+
+//Codeforce Problem No - 4A
+
+//Problem Name - Watermelon
+
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d", &x);
+    if( x > 2 && x%2==0){
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+    return 0;
+}
