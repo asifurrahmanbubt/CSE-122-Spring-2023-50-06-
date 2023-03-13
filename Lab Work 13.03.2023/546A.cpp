4a4fc156d5749d142c2dd398d16b13528194d849
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-13 11:06:09 +0600

    Add files via upload

---
+++ Lab Work 13.03.2023/546A.cpp
@@ -0,0 +1,24 @@
+//Soldier and Bananas
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int k, n, w;
+    cin >> k >> n >> w;
+
+    int cost = 0;
+    for (int i = 1; i <= w; i++) {
+        cost += i * k;
+    }
+
+    int borrow = cost - n;
+    if (borrow < 0) {
+        borrow = 0;
+    }
+
+    cout << borrow << endl;
+
+    return 0;
+}
